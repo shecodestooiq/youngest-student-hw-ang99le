f44d8c9b038f4609971ede6d777f2a759ac9cb57
ang99le <malak.d993@gmail.com>
2024-02-12 23:00:26 +0300

    youngestStudent

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function showYoungestStudent(students) { }
-
+function showYoungestStudent(userDefinedStudents) {
+    let min=userDefinedStudents[1];
+   for(let i=0 ;i < userDefinedStudents.length ; i++){
+       
+   if (userDefinedStudents[i]<min)
+   {
+      min=userDefinedStudents[i]; 
+   }
+ 
+   } 
+   console.log("the youngest student is : ", min);
+};
+const userDefinedStudents = [
+   { name: 'Alice', age: 25 },
+   { name: 'Bob', age: 2 },
+   { name: 'Charlie', age: 21 },
+];
+console.log(userDefinedStudents);
+showYoungestStudent(userDefinedStudents);
 module.exports = showYoungestStudent;
